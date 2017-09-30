40620549ffa2274e3eb9af43c5c9192e982f66f8
Ardalan Amini <ardalan1074@gmail.com>
2017-09-30 21:15:22 +0330

    updated fox version to 0.1.0-alpha.6

--- fox-cli.js
+++ fox-cli.js
@@ -167,7 +167,7 @@ function createApplication (name, path) {
         start: 'babel-node ./app.js',
       },
       dependencies: {
-        'fox': '0.1.0-alpha.5',
+        'fox': '0.1.0-alpha.6',
         'core-decorators': '^0.20.0',
       },
       devDependencies: {
